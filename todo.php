c58ae67df6fb9c3ab93b38667d856d966882dde9
Echo-007-sudo <bosspofii@gmail.com>
2026-07-22 03:51:42 +0100

    variables in php

---
+++ todo.php
@@ -0,0 +1,16 @@
+<?php
+$todos = [
+    "Learn PHP basics",
+    "Set up nginx",
+    "Build a todo app"
+];
+
+// echo "My Todos:\n";
+// foreach ($todos as $index => $todo) {
+//     echo ($index + 1) . ". " . $todo . "\n";
+// }
+echo "Hello, World!\n";
+
+$status = "404" ;
+echo $status;
+var_dump($status);
